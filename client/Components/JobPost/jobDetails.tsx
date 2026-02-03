c14926ad87a4a37e9636e28c438dbdb4a4264666
Anshul Srivastava <anshulakgec@gmail.com>
2026-02-03 23:25:59 +0530

    commit

--- client/Components/JobPost/jobDetails.tsx
+++ client/Components/JobPost/jobDetails.tsx
@@ -1,8 +1,39 @@
+"use client"
+import { useGlobalContext } from '@/context/globalContext'
 import React from 'react'
+import ReactQuill from 'react-quill-new'
+import { Label } from '../ui/label'
+import 'react-quill-new/dist/quill.snow.css'
+
+function MyEditor(){
+  const{jobDescription,setJobDescription}=useGlobalContext()
+  return <ReactQuill value={jobDescription} onChange={setJobDescription}
+  style={{minHeight:"200px",maxHeight:"900px",}}
+  
+  modules={{
+    toolbar:true 
+  }}
+  className='custom-quill-editor'/>
+}
 
 function jobDetails() {
   return (
-    <div>jobDetails</div>
+    <div className='p-6 flex flex-col gap-4 bg-background border border-border rounded-lg'>
+      <div className='grid grid-cols-2 gap-6'>
+        <div className='flex-1'>
+          <h3 className='text-black font-bold'>Job Description</h3>
+          <Label htmlFor='jobDescription' className='text-gray-500 mt-2'>
+          Provide a detailed description of the job.
+          </Label>
+        </div>
+        <div className='flex-1'>
+          <MyEditor/>
+
+        </div>
+
+      </div>
+   
+    </div>
   )
 }
 
