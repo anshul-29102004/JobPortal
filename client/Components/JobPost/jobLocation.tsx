2a923ad1e6a6d11d7df4edb5946b67b5487a13ad
Anshul Srivastava <anshulakgec@gmail.com>
2026-02-04 23:15:17 +0530

    Commit

--- client/Components/JobPost/jobLocation.tsx
+++ client/Components/JobPost/jobLocation.tsx
@@ -1,8 +1,12 @@
+import { useGlobalContext } from '@/context/globalContext'
 import React from 'react'
 
 function jobLocation() {
+  const {location,setLocation}=useGlobalContext();
   return (
-    <div>jobLocation</div>
+    <div>
+
+    </div>
   )
 }
 
